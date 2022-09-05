448be66bd730a4e3b965f1c26e4e7d1add381ddb
Peter Munch <peterrmuench@gmail.com>
2022-09-05 19:22:51 +0200

    Add transfer experiment

---
+++ experiments/transfer.sh
@@ -0,0 +1,5 @@
+mpirun -np 40 ./transfer_01 3 2 1 48 | tee experiment-transfer/output_2.out
+mpirun -np 40 ./transfer_01 3 3 1 46 | tee experiment-transfer/output_3.out
+mpirun -np 40 ./transfer_01 3 4 1 44 | tee experiment-transfer/output_4.out
+mpirun -np 40 ./transfer_01 3 5 1 42 | tee experiment-transfer/output_5.out
+mpirun -np 40 ./transfer_01 3 6 1 40 | tee experiment-transfer/output_6.out
