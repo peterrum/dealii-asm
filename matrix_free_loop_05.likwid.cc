b7789a4550fca40e4bd4438aca484a5d3082dfec
Peter Munch <peterrmuench@gmail.com>
2022-09-05 20:57:21 +0200

    Continue

--- matrix_free_loop_05.likwid.cc
+++ matrix_free_loop_05.likwid.cc
@@ -91,6 +91,29 @@ public:
   }
 
   void
+  vmult(VectorType &      dst,
+        const VectorType &src,
+        const std::function<void(const unsigned int, const unsigned int)>
+          &operation_before_matrix_vector_product,
+        const std::function<void(const unsigned int, const unsigned int)>
+          &operation_after_matrix_vector_product) const
+  {
+    matrix_free.template cell_loop<VectorType, VectorType>(
+      [&](const auto &, auto &dst, const auto &src, const auto cells) {
+        FECellIntegrator phi(matrix_free);
+        for (unsigned int cell = cells.first; cell < cells.second; ++cell)
+          {
+            phi.reinit(cell);
+            do_cell_integral_global(phi, dst, src);
+          }
+      },
+      dst,
+      src,
+      operation_before_matrix_vector_product,
+      operation_after_matrix_vector_product);
+  }
+
+  void
   initialize_dof_vector(VectorType &vec) const
   {
     matrix_free.initialize_dof_vector(vec);
@@ -107,6 +130,12 @@ public:
 
     for (auto &i : diagonal)
       i = (std::abs(i) > 1.0e-10) ? (1.0 / i) : 1.0;
+  }
+
+  const AffineConstraints<Number> &
+  get_constraints() const
+  {
+    return matrix_free.get_affine_constraints();
   }
 
 private:
@@ -134,13 +163,150 @@ private:
   }
 };
 
-template <int dim, typename Number>
+
+template <typename VectorType>
+class DiagonalMatrixPrePost : public Subscriptor
+{
+public:
+  DiagonalMatrixPrePost(const DiagonalMatrix<VectorType> &op)
+    : op(op)
+  {}
+
+  void
+  vmult(VectorType &dst, const VectorType &src) const
+  {
+    op.vmult(dst, src);
+  }
+
+  void
+  vmult(VectorType &      dst,
+        const VectorType &src,
+        const std::function<void(const unsigned int, const unsigned int)>
+          &operation_before_matrix_vector_product,
+        const std::function<void(const unsigned int, const unsigned int)>
+          &operation_after_matrix_vector_product) const
+  {
+    const auto dst_ptr  = dst.begin();
+    const auto src_ptr  = src.begin();
+    const auto diag_ptr = op.get_vector().begin();
+
+    const auto locally_owned_size = dst.locally_owned_size();
+
+    for (unsigned int i = 0; i < locally_owned_size; i += 100)
+      {
+        const unsigned int begin = i;
+        const unsigned int end   = std::min(begin + 100, locally_owned_size);
+
+        operation_before_matrix_vector_product(begin, end);
+
+        for (unsigned int j = begin; j < end; ++j)
+          dst_ptr[j] = src_ptr[j] * diag_ptr[j];
+
+        operation_after_matrix_vector_product(begin, end);
+      }
+  }
+
+private:
+  const DiagonalMatrix<VectorType> &op;
+};
+
+
+template <typename VectorType>
+class DiagonalMatrixAdapter : public Subscriptor
+{
+public:
+  DiagonalMatrixAdapter(const DiagonalMatrix<VectorType> &op)
+    : op(op)
+  {}
+
+  void
+  vmult(VectorType &dst, const VectorType &src) const
+  {
+    op.vmult(dst, src);
+  }
+
+private:
+  const DiagonalMatrix<VectorType> &op;
+};
+
+
+
+template <typename OperatorType, typename PreconditionerType>
 void
-test(const unsigned int fe_degree, const unsigned int n_subdivision)
+run_chebyshev(const OperatorType &                      op,
+              const std::shared_ptr<PreconditionerType> precon_diag)
 {
   const bool chebyshev_degree = 3;    // TODO
   const bool do_vmult         = true; // TODO
 
+  using VectorType = typename OperatorType::VectorType;
+
+  typename PreconditionChebyshev<OperatorType, VectorType, PreconditionerType>::
+    AdditionalData chebyshev_ad;
+
+  chebyshev_ad.preconditioner = precon_diag;
+  chebyshev_ad.constraints.copy_from(op.get_constraints());
+  chebyshev_ad.degree = chebyshev_degree;
+
+  PreconditionChebyshev<OperatorType, VectorType, PreconditionerType>
+    precon_chebyshev;
+
+  precon_chebyshev.initialize(op, chebyshev_ad);
+
+  VectorType src, dst;
+
+  op.initialize_dof_vector(src);
+  op.initialize_dof_vector(dst);
+  src = 1;
+
+  if (do_vmult)
+    precon_chebyshev.vmult(dst, src);
+  else
+    precon_chebyshev.step(dst, src);
+}
+
+
+
+template <typename OperatorType, typename PreconditionerType>
+void
+run_relaxation(const OperatorType &                      op,
+               const std::shared_ptr<PreconditionerType> precon_diag)
+{
+  const bool chebyshev_degree = 3;    // TODO
+  const bool do_vmult         = true; // TODO
+
+  using VectorType = typename OperatorType::VectorType;
+
+  typename PreconditionRelaxation<OperatorType,
+                                  PreconditionerType>::AdditionalData
+    relaxation_ad;
+
+  relaxation_ad.preconditioner = precon_diag;
+  relaxation_ad.n_iterations   = chebyshev_degree;
+  relaxation_ad.relaxation     = 1.1;
+
+  PreconditionRelaxation<OperatorType, PreconditionerType> precon_relaxation;
+
+  precon_relaxation.initialize(op, relaxation_ad);
+
+  VectorType src, dst;
+
+  op.initialize_dof_vector(src);
+  op.initialize_dof_vector(dst);
+  src = 1;
+
+  if (do_vmult)
+    precon_relaxation.vmult(dst, src);
+  else
+    precon_relaxation.step(dst, src);
+}
+
+
+
+template <int dim, typename Number>
+void
+test(const unsigned int fe_degree, const unsigned int n_subdivision)
+{
   using VectorizedArrayType = VectorizedArray<Number>;
   using VectorType          = LinearAlgebra::distributed::Vector<Number>;
 
@@ -178,30 +344,40 @@ test(const unsigned int fe_degree, const unsigned int n_subdivision)
   const auto precon_diag = std::make_shared<DiagonalMatrix<VectorType>>();
   op.compute_inverse_diagonal(precon_diag->get_vector());
 
-  typename PreconditionChebyshev<OperatorType,
-                                 VectorType,
-                                 DiagonalMatrix<VectorType>>::AdditionalData
-    chebyshev_ad;
+  const auto precon_pre_post =
+    std::make_shared<DiagonalMatrixPrePost<VectorType>>(*precon_diag);
 
-  chebyshev_ad.preconditioner = precon_diag;
-  chebyshev_ad.constraints.copy_from(constraints);
-  chebyshev_ad.degree = chebyshev_degree;
+  const auto precon_adapter =
+    std::make_shared<DiagonalMatrixAdapter<VectorType>>(*precon_diag);
 
-  PreconditionChebyshev<OperatorType, VectorType, DiagonalMatrix<VectorType>>
-    precon_chebyshev;
-
-  precon_chebyshev.initialize(op, chebyshev_ad);
-
-  VectorType src, dst;
-
-  op.initialize_dof_vector(src);
-  op.initialize_dof_vector(dst);
-  src = 1;
-
-  if (do_vmult)
-    precon_chebyshev.vmult(dst, src);
+  if (true)
+    {
+      run_chebyshev(op, precon_adapter);
+    }
+  else if (true)
+    {
+      run_chebyshev(op, precon_pre_post);
+    }
+  else if (true)
+    {
+      run_chebyshev(op, precon_diag);
+    }
+  else if (true)
+    {
+      run_relaxation(op, precon_adapter);
+    }
+  else if (true)
+    {
+      run_relaxation(op, precon_pre_post);
+    }
+  else if (true)
+    {
+      run_relaxation(op, precon_diag);
+    }
   else
-    precon_chebyshev.step(dst, src);
+    {
+      AssertThrow(false, ExcNotImplemented());
+    }
 }
 
 
