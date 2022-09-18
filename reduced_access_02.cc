ec43a4942590f2a6f2b0d3e8c5569f5c9f590e0b
Peter Munch <peterrmuench@gmail.com>
2022-09-18 21:08:12 +0200

    Work on 3D

--- reduced_access_02.cc
+++ reduced_access_02.cc
@@ -192,6 +192,177 @@ gather(const std::vector<Number> &      global_vector,
     }
 }
 
+template <typename Number>
+void
+gather_post(const std::vector<Number> &      global_vector,
+            const unsigned int               degree,
+            const std::vector<unsigned int> &dofs_of_cell,
+            const std::vector<unsigned int> &orientations, // TODO: compress
+            const Table<2, unsigned int> &   orientation_table,
+            std::vector<Number> &            local_vector)
+{
+  std::vector<std::pair<unsigned int, unsigned int>> orientations_{
+    // lines - x
+    {1, orientations[2]},
+    {1, orientations[3]},
+    {1, orientations[6]},
+    {1, orientations[7]},
+    // lines - y
+    {1, orientations[0]},
+    {1, orientations[1]},
+    {1, orientations[4]},
+    {1, orientations[5]},
+    // lines - z
+    {1, orientations[8]},
+    {1, orientations[9]},
+    {1, orientations[10]},
+    {1, orientations[11]},
+    // quads
+    {3, orientations[12]},
+    {3, orientations[13]},
+    {3, orientations[14]},
+    {3, orientations[15]},
+    {3, orientations[16]},
+    {3, orientations[17]}};
+
+  unsigned int orientation = 0;
+
+  for (unsigned int i = 0, s = 0; i < orientations_.size(); ++i)
+    {
+      orientation += orientations_[i].second << s;
+      s += orientations_[i].first;
+    }
+
+  for (unsigned int k = 0, compressed_k = 0, offset_k = 0, c = 0; k <= degree;
+       ++k)
+    {
+      for (unsigned int j = 0, compressed_j = 0, offset_j = 0; j <= degree; ++j)
+        {
+          const unsigned int offset =
+            (compressed_j == 1 ? degree - 1 : 1) * offset_k + offset_j;
+
+          const auto indices =
+            dofs_of_cell.begin() + 3 * (compressed_k * 3 + compressed_j);
+
+          local_vector[c++] = global_vector[indices[0] + offset];
+
+          for (unsigned int i = 0; i < degree - 1; ++i)
+            local_vector[c++] =
+              global_vector[indices[1] + offset * (degree - 1) + i];
+
+          local_vector[c++] = global_vector[indices[2] + offset];
+
+          if (j == 0 || j == degree - 1)
+            {
+              ++compressed_j;
+              offset_j = 0;
+            }
+          else
+            {
+              ++offset_j;
+            }
+        }
+
+      if (k == 0 || k == degree - 1)
+        {
+          ++compressed_k;
+          offset_k = 0;
+        }
+      else
+        {
+          ++offset_k;
+        }
+    }
+
+  if (orientation != 0)
+    {
+      for (unsigned int l = 0; l < 12; ++l) // loop over all lines
+        {
+          if (orientation & 1) // check bit
+            {
+              // determine stride and begin
+              const unsigned int stride = Utilities::pow(degree + 1, l / 4);
+
+              unsigned int begin = 0;
+
+              if (l == 0)
+                begin = 1;
+              else if (l == 2)
+                begin = (degree + 1) * degree + 1;
+              else if (l == 3)
+                begin = (degree + 1) * (degree + 1) * degree + 1;
+              else if (l == 4)
+                begin = (degree + 1) * (degree + 1) * degree +
+                        (degree + 1) * degree + 1;
+              else if (l == 5)
+                begin = (degree + 1);
+              else if (l == 6)
+                begin = (degree + 1) + degree;
+              else if (l == 7)
+                begin = (degree + 1) * (degree + 1) * degree + (degree + 1);
+              else if (l == 8)
+                begin =
+                  (degree + 1) * (degree + 1) * degree + (degree + 1) + degree;
+              else if (l == 9)
+                begin = (degree + 1) * (degree + 1);
+              else if (l == 10)
+                begin = (degree + 1) * (degree + 1) + degree;
+              else if (l == 11)
+                begin =
+                  (degree + 1) * (degree + 1) * degree + (degree + 1) * degree;
+              else if (l == 12)
+                begin = (degree + 1) * (degree + 1) * degree +
+                        (degree + 1) * degree + degree;
+
+              // perform reorientation
+              for (unsigned int i = 0; i < (degree - 1) / 2; ++i)
+                std::swap(local_vector[begin + i * stride],
+                          local_vector[begin + (degree - 2 - i) * stride]);
+            }
+
+          orientation = orientation >> 1; //  go to next bit
+        }
+
+      for (unsigned int q = 0; q < 6; ++q) // loop over all quads
+        {
+          const unsigned int flag = orientation & 0b111;
+
+          if (flag != 0) // check bits
+            {
+              Number temp[100];
+
+              const unsigned int d      = q / 2;
+              const unsigned int stride = (d == 0) ? (degree + 1) : 1;
+              const unsigned int stride2 =
+                (d == 2) ? (degree + 1) : ((degree + 1) * (degree + 1));
+
+              unsigned int begin = 0;
+              if ((q % 2) == 0)
+                begin = 0;
+              else if (q == 1)
+                begin = degree;
+              else if (q == 3)
+                begin = (degree + 1) * degree;
+              else if (q == 5)
+                begin = (degree + 1) * (degree + 1) * degree;
+
+              // copy values into buffer
+              for (unsigned int g = 1, c = 0; g < degree; ++g)
+                for (unsigned int k = 1; k < degree; ++k, ++c)
+                  temp[c] = local_vector[begin + k * stride + stride2 * g];
+
+              // perform permuation
+              for (unsigned int g = 1, c = 0; g < degree; ++g)
+                for (unsigned int k = 1; k < degree; ++k, ++c)
+                  local_vector[begin + k * stride + stride2 * g] =
+                    temp[orientation_table[flag][c]];
+            }
+
+          orientation = orientation >> 3; //  go to next bits
+        }
+    }
+}
+
 /**
  * ./reduced_access_02 3
  * ./reduced_access_02 3   0 0 0 0 0 0 0 0 0 0 0 0   0 0 0 0 0 1
@@ -260,6 +431,13 @@ main(int argc, char *argv[])
          orientations,
          orientation_table,
          local_vector);
+
+  gather_post(global_vector,
+              degree,
+              dofs_of_cell,
+              orientations,
+              orientation_table,
+              local_vector);
 
   for (unsigned int k = 0, c = 0; k <= degree; ++k)
     {
