3aae518b6845a41e046874feaa461c9fbc0fe69a
Peter Munch <peterrmuench@gmail.com>
2022-09-19 13:43:20 +0200

    Switch

--- include/tensor_product_matrix.h
+++ include/tensor_product_matrix.h
@@ -332,43 +332,55 @@ namespace dealii
                   const ArrayView<const Number> &src_in,
                   AlignedVector<Number> &        tmp_array) const
     {
-      const auto do_apply = [&](const auto &fdm) {
-        Number *      dst = dst_in.begin();
-        const Number *src = src_in.begin();
+      Number *      dst = dst_in.begin();
+      const Number *src = src_in.begin();
 
-        std::array<const Number *, dim> eigenvectors, eigenvalues;
-
-        for (unsigned int d = 0; d < dim; ++d)
-          {
-            eigenvectors[d] = &fdm.get_eigenvectors()[d](0, 0);
-            eigenvalues[d]  = fdm.get_eigenvalues()[d].data();
-          }
-
-        const unsigned int n_rows_1d_non_templated =
-          fdm.get_eigenvalues()[0].size();
-
-        if (n_rows_1d != -1)
-          internal::TensorProductMatrixSymmetricSum::apply_inverse<
-            n_rows_1d == -1 ? 0 : n_rows_1d>(dst,
-                                             src,
-                                             tmp_array,
-                                             n_rows_1d_non_templated,
-                                             eigenvectors,
-                                             eigenvalues);
-        else
-          internal::TensorProductMatrixSymmetricSum::select_apply_inverse<1>(
-            dst,
-            src,
-            tmp_array,
-            n_rows_1d_non_templated,
-            eigenvectors,
-            eigenvalues);
-      };
+      std::array<const Number *, dim> eigenvectors, eigenvalues;
+      unsigned int                    n_rows_1d_non_templated = 0;
 
       if (compressed_vector.size() > 0)
-        do_apply(compressed_vector[indices[index]]);
+        {
+          // TODO
+          const auto &fdm = compressed_vector[indices[index]];
+
+          for (unsigned int d = 0; d < dim; ++d)
+            {
+              eigenvectors[d] = &fdm.get_eigenvectors()[d](0, 0);
+              eigenvalues[d]  = fdm.get_eigenvalues()[d].data();
+            }
+
+          n_rows_1d_non_templated = fdm.get_eigenvalues()[0].size();
+        }
       else
-        do_apply(vector[index]);
+        {
+          // TODO
+          const auto &fdm = vector[index];
+
+          for (unsigned int d = 0; d < dim; ++d)
+            {
+              eigenvectors[d] = &fdm.get_eigenvectors()[d](0, 0);
+              eigenvalues[d]  = fdm.get_eigenvalues()[d].data();
+            }
+
+          n_rows_1d_non_templated = fdm.get_eigenvalues()[0].size();
+        }
+
+      if (n_rows_1d != -1)
+        internal::TensorProductMatrixSymmetricSum::apply_inverse<
+          n_rows_1d == -1 ? 0 : n_rows_1d>(dst,
+                                           src,
+                                           tmp_array,
+                                           n_rows_1d_non_templated,
+                                           eigenvectors,
+                                           eigenvalues);
+      else
+        internal::TensorProductMatrixSymmetricSum::select_apply_inverse<1>(
+          dst,
+          src,
+          tmp_array,
+          n_rows_1d_non_templated,
+          eigenvectors,
+          eigenvalues);
     }
 
     std::size_t
