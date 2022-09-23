8b3e28751536f90e5ecfb5ea8263b8b7ff0b703a
Peter Munch <peterrmuench@gmail.com>
2022-09-23 22:12:04 +0200

    Continue

--- matrix_free_loop_06.likwid.cc
+++ matrix_free_loop_06.likwid.cc
@@ -26,7 +26,8 @@ struct Parameters
 {
   unsigned int fe_degree           = 3;
   unsigned int n_subdivision       = 1;
-  std::string  preconditioner_type = "relaxation";
+  std::string  type                = "Chebyshev";
+  std::string  preconditioner_type = "Diagonal";
   unsigned int optimization_level  = 2;
   bool         do_vmult            = true;
 
@@ -62,9 +63,10 @@ test(const Parameters params_in)
   MappingQ1<dim> mapping;
 
   parallel::distributed::Triangulation<dim> tria(MPI_COMM_WORLD);
-  tria.refine_global(
+  const unsigned int                        n_global_refinements =
     GridGenerator::subdivided_hyper_cube_balanced(tria,
-                                                  params_in.n_subdivision));
+                                                  params_in.n_subdivision,
+                                                  true);
 
   std::vector<
     GridTools::PeriodicFacePair<typename Triangulation<dim>::cell_iterator>>
@@ -73,6 +75,8 @@ test(const Parameters params_in)
     GridTools::collect_periodic_faces(
       tria, 2 * d, 2 * d + 1, d, periodic_faces);
   tria.add_periodicity(periodic_faces);
+
+  tria.refine_global(n_global_refinements);
 
   DoFHandler<dim> dof_handler(tria);
   dof_handler.distribute_dofs(fe);
@@ -106,44 +110,77 @@ test(const Parameters params_in)
 
   LaplaceOperatorMatrixFree<dim, Number> op(matrix_free, ad_operator);
 
-  // create preconditioner
-
-  boost::property_tree::ptree params; // TODO: fill
-  (void)params_in.preconditioner_type;
-  (void)params_in.optimization_level;
-  (void)params_in.max_degree;
-
-  const auto precondition = create_system_preconditioner(op, params);
-
   VectorType src, dst;
 
   op.initialize_dof_vector(src);
   op.initialize_dof_vector(dst);
 
-  for (unsigned int i = 0; i < params_in.n_repetitions; ++i)
+  src = 1.0;
+
+  // loop over a range of smoothing degrees
+  for (unsigned int degree = 1; degree <= params_in.max_degree; ++degree)
     {
-      (void)params_in.do_vmult;
-      precondition->vmult(dst, src);
+      // create preconditioner
+      boost::property_tree::ptree params;
+
+      params.put("type", params_in.type);
+      params.put("degree", std::to_string(degree));
+      params.put("optimize", params_in.optimization_level);
+      params.put("preconditioner.type", params_in.preconditioner_type);
+
+      const auto precondition = create_system_preconditioner(op, params);
+
+      // warm up
+      for (unsigned int i = 0; i < params_in.n_repetitions; ++i)
+        {
+          (void)params_in.do_vmult;
+          precondition->vmult(dst, src);
+        }
+
+      // time vmult
+      const std::string label = params_in.type + "_" +
+                                params_in.preconditioner_type + "_" +
+                                std::to_string(likwid_counter);
+
+      MPI_Barrier(MPI_COMM_WORLD);
+      LIKWID_MARKER_START(label.c_str());
+
+      for (unsigned int i = 0; i < params_in.n_repetitions; ++i)
+        {
+          (void)params_in.do_vmult;
+          precondition->vmult(dst, src);
+        }
+
+      MPI_Barrier(MPI_COMM_WORLD);
+      LIKWID_MARKER_STOP(label.c_str());
+
+      likwid_counter++;
     }
-
-  const std::string label =
-    params_in.preconditioner_type + "_" + std::to_string(likwid_counter);
-
-  MPI_Barrier(MPI_COMM_WORLD);
-  LIKWID_MARKER_START(label.c_str());
-
-  for (unsigned int i = 0; i < params_in.n_repetitions; ++i)
-    {
-      (void)params_in.do_vmult;
-      precondition->vmult(dst, src);
-    }
-
-  MPI_Barrier(MPI_COMM_WORLD);
-  LIKWID_MARKER_STOP(label.c_str());
-
-  likwid_counter++;
 }
 
+
+/**
+// clang-format off
+Test relaxation:
+likwid-mpirun -np 40 -f -g CACHES -m -O ./matrix_free_loop_06 3 4 40 1 Relaxation Diagonal 0 1 | tee temp
+likwid-mpirun -np 40 -f -g CACHES -m -O ./matrix_free_loop_06 3 4 40 1 Relaxation Diagonal 1 1 | tee temp
+likwid-mpirun -np 40 -f -g CACHES -m -O ./matrix_free_loop_06 3 4 40 1 Relaxation Diagonal 2 1 | tee temp
+likwid-mpirun -np 40 -f -g CACHES -m -O ./matrix_free_loop_06 3 4 40 1 Relaxation Diagonal 3 1 | tee temp
+
+Test Chebyshev:
+likwid-mpirun -np 40 -f -g CACHES -m -O ./matrix_free_loop_06 3 4 40 1 Chebyshev Diagonal 0 1 | tee temp
+likwid-mpirun -np 40 -f -g CACHES -m -O ./matrix_free_loop_06 3 4 40 1 Chebyshev Diagonal 1 1 | tee temp
+likwid-mpirun -np 40 -f -g CACHES -m -O ./matrix_free_loop_06 3 4 40 1 Chebyshev Diagonal 2 1 | tee temp
+likwid-mpirun -np 40 -f -g CACHES -m -O ./matrix_free_loop_06 3 4 40 1 Chebyshev Diagonal 3 1 | tee temp
+
+Postprocess each file with:
+cat test | grep "Memory read data volume" | awk 'NR % 2 == 0'
+echo ""
+cat test | grep "Memory write data volume" | awk 'NR % 2 == 0'
+echo ""
+echo ""
+ // clang-format on
+ */
 int
 main(int argc, char *argv[])
 {
@@ -158,14 +195,17 @@ main(int argc, char *argv[])
   const unsigned int fe_degree     = (argc >= 3) ? std::atoi(argv[2]) : 1;
   const unsigned int n_subdivision = (argc >= 4) ? std::atoi(argv[3]) : 6;
   const bool         use_float     = (argc >= 5) ? std::atoi(argv[4]) : 1;
-  const std::string  preconditioner_type =
-    (argc >= 6) ? std::string(argv[5]) : std::string("chebyshev");
-  const unsigned int optimization_level = (argc >= 7) ? std::atoi(argv[6]) : 2;
-  const bool         do_vmult           = (argc >= 8) ? std::atoi(argv[7]) : 1;
+  const std::string  type =
+    (argc >= 6) ? std::string(argv[5]) : std::string("Chebyshev");
+  const std::string preconditioner_type =
+    (argc >= 7) ? std::string(argv[6]) : std::string("Diagonal");
+  const unsigned int optimization_level = (argc >= 8) ? std::atoi(argv[7]) : 3;
+  const bool         do_vmult           = (argc >= 9) ? std::atoi(argv[8]) : 1;
 
   Parameters params;
   params.fe_degree           = fe_degree;
   params.n_subdivision       = n_subdivision;
+  params.type                = type;
   params.preconditioner_type = preconditioner_type;
   params.optimization_level  = optimization_level;
   params.do_vmult            = do_vmult;
