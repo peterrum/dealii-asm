918e80a0b150e79b13dc19a1f2822888822e6d3b
Peter Munch <peterrmuench@gmail.com>
2022-10-04 11:08:44 +0200

    Skripts for comparison

---
+++ experiments/matrix_free_loop_08_1.py
@@ -0,0 +1,28 @@
+
+def main():
+    configurations = []
+
+    for k in [1, 2]:
+        for type in ["c", "l", "dg", "g-s-c", "g-s-n", "g-p-c", "g-p-n"]:
+            for op in ["add", "none", "post", "pre", "symm"]:
+                predicate = False
+                if k == 1 and type == "c" and (op == "post" or op == "pre" or op == "symm"):
+                    predicate = True
+                elif (type == "l" or type == "dg") and (op == "post" or op == "pre" or op == "symm"):
+                    predicate = True
+                elif (type == "g-s-c" or type == "g-p-c") and (op == "pre" or op == "symm"):
+                    predicate = True
+                elif type == "g-s-n" and (op == "none" or op == "post" or op == "pre" or op == "symm"):
+                    predicate = True
+                elif type == "g-p-n":
+                    predicate = True
+
+                if predicate:
+                    configurations.append("%s-%d-%s" % (op, k, type))
+
+    print(" ".join(configurations))
+
+    print(len(configurations))
+
+if __name__== "__main__":
+  main()
