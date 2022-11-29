5a30fa37ee1f78c28bb33f37441620b41f2ca345
Peter Munch <peterrmuench@gmail.com>
2022-11-29 16:06:25 +0100

    Prepare layers

--- overlap_unstructured_01.cc
+++ overlap_unstructured_01.cc
@@ -97,15 +97,23 @@ namespace dealii
   } // namespace GridGenerator
 } // namespace dealii
 
-int
-main()
+template <int dim>
+void
+test(const int fe_degree, const unsigned int n_overlap, const int orientation)
 {
-  const int dim       = 2;
-  const int fe_degree = 2;
-  const int n_overlap = 1;
-
   Triangulation<dim> tria;
-  GridGenerator::my_non_standard_orientation_mesh(tria, 0, 1);
+
+  if constexpr (dim == 2)
+    {
+      GridGenerator::my_non_standard_orientation_mesh(tria, 0, orientation);
+    }
+  else if constexpr (dim == 3)
+    {
+      GridGenerator::non_standard_orientation_mesh(
+        tria, !(orientation & 1), orientation & 2, orientation & 4, false);
+    }
+  else
+    AssertThrow(false, ExcNotImplemented());
 
   FE_DGQ<dim> fe(fe_degree);
   QGauss<dim> quad(fe_degree + 1);
@@ -113,15 +121,16 @@ main()
   DoFHandler<dim> dof_handler(tria);
   dof_handler.distribute_dofs(fe);
 
-  const unsigned int n_dofs_per_cell = Utilities::pow(fe_degree + 1, dim);
-  const unsigned int n_dofs_per_face =
-    n_overlap * Utilities::pow(fe_degree + 1, dim - 1);
+  const unsigned int n_dofs_per_cell  = Utilities::pow(fe_degree + 1, dim);
+  const unsigned int n_dofs_per_layer = Utilities::pow(fe_degree + 1, dim - 1);
+  const unsigned int n_dofs_per_face  = n_overlap * n_dofs_per_layer;
 
   std::vector<types::global_dof_index> dof_indices_face(n_dofs_per_face);
-  std::vector<types::global_dof_index> dof_indices_temp(n_dofs_per_face);
+  std::vector<types::global_dof_index> dof_indices_temp(n_dofs_per_layer);
   std::vector<types::global_dof_index> dof_indices(n_dofs_per_cell);
 
   internal::MatrixFreeFunctions::ShapeInfo<double> shape_info(quad, fe);
+  const auto &face_to_cell_index_nodal = shape_info.face_to_cell_index_nodal;
 
   const auto get_face_indices_of_neighbor = [&](const auto &cell,
                                                 const auto  face_no,
@@ -135,7 +144,7 @@ main()
 
     for (unsigned int i = 0; i < n_dofs_per_face; ++i)
       dof_indices_face[i] =
-        dof_indices[shape_info.face_to_cell_index_nodal[exterior_face_no][i]];
+        dof_indices[face_to_cell_index_nodal[exterior_face_no][i]];
 
     for (unsigned int i = 0; i < n_dofs_per_cell; ++i)
       dof_indices[i] = 0;
@@ -147,18 +156,20 @@ main()
           2 * neighbor->face_flip(exterior_face_no) +
           4 * neighbor->face_rotation(exterior_face_no);
 
-        for (unsigned int i = 0; i < n_dofs_per_face; ++i)
-          dof_indices_temp
-            [shape_info.face_orientations_quad[exterior_face_orientation][i]] =
-              dof_indices_face[i];
+        for (unsigned int l = 0; l < n_overlap; ++l)
+          {
+            for (unsigned int i = 0; i < n_dofs_per_layer; ++i)
+              dof_indices_temp[shape_info.face_orientations_quad
+                                 [exterior_face_orientation][i]] =
+                dof_indices_face[i + l * n_dofs_per_layer];
 
-        for (unsigned int i = 0; i < n_dofs_per_face; ++i)
-          dof_indices_face[i] = dof_indices_temp[i];
+            for (unsigned int i = 0; i < n_dofs_per_layer; ++i)
+              dof_indices_face[i + l * n_dofs_per_layer] = dof_indices_temp[i];
+          }
       }
 
     for (unsigned int i = 0; i < n_dofs_per_face; ++i)
-      dof_indices[shape_info.face_to_cell_index_nodal[face_no][i]] =
-        dof_indices_face[i];
+      dof_indices[face_to_cell_index_nodal[face_no][i]] = dof_indices_face[i];
   };
 
   for (const auto &cell : dof_handler.active_cell_iterators())
@@ -166,7 +177,7 @@ main()
       for (const auto face_index : cell->face_indices())
         if (cell->at_boundary(face_index) == false)
           {
-            std::cout << cell->face_orientation(face_index) << std::endl;
+            std::cout << face_index << std::endl;
 
             get_face_indices_of_neighbor(cell, face_index, dof_indices);
 
@@ -176,4 +187,22 @@ main()
           }
       std::cout << std::endl;
     }
+}
+
+int
+main(int argc, char *argv[])
+{
+  AssertThrow(argc == 5, ExcNotImplemented());
+
+  const int dim         = std::atoi(argv[1]);
+  const int fe_degree   = std::atoi(argv[2]);
+  const int n_overlap   = std::atoi(argv[3]);
+  const int orientation = std::atoi(argv[4]);
+
+  if (dim == 2)
+    test<2>(fe_degree, n_overlap, orientation);
+  else if (dim == 3)
+    test<3>(fe_degree, n_overlap, orientation);
+  else
+    AssertThrow(false, ExcNotImplemented());
 }
